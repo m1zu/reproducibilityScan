e61d8731f71b7850c4f7fc4e8c4b9fbb61fdc20f
Damian Iwanicki <damian.iwanicki@rwth-aachen.de>
2018-01-09 16:18:28 +0100

    v1.0

--- main.cc
+++ main.cc
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
   //TH1::AddDirectory(false);
   Helpers::setRootStyle();
 
-  QFile textFile("/home/iwanicki/sw/opticalCon/data/4TSAACFIM00127/scan/short.csv");
+  QFile textFile("/home/iwanicki/sw/opticalCon/data/4TSAACFIM00127/reproducibility_scan/1.csv");
   int count=0;
   if (textFile.open(QIODevice::ReadOnly)) {
       QTextStream stream(&textFile);
@@ -24,57 +24,106 @@ int main(int argc, char** argv) {
   }
   textFile.close();
 
-  const int nFibresShort = count;
-  TGraph* gs = new TGraph(nFibresShort);
+  const int nFibres = count;
+  TGraph* g0 = new TGraph(nFibres);
+  TGraph* g1 = new TGraph(nFibres);
+  TGraph* g2 = new TGraph(nFibres);
+  TGraph* g3 = new TGraph(nFibres);
+  TGraph* g4 = new TGraph(nFibres);
+
   count = 0;
   if (textFile.open(QIODevice::ReadOnly)) {
       QTextStream stream(&textFile);
       while (!stream.atEnd())
       {
           coordinate fibre;
-          double temp;
-          stream >> fibre.number >> temp
+          double dump;
+          stream >> fibre.number >> dump
                     >> fibre.x >> fibre.y;
-          qDebug() << "shortFiber" << fibre.number << fibre.x << fibre.y;
+          //qDebug() << "shortFiber" << fibre.number << fibre.x << fibre.y;
           if (fibre.number != (count+1))
               qDebug() << "fibre " << count << " skipped!";
-          gs->SetPoint(fibre.number-1, (fibre.x*0.001), (fibre.y+4800.));
+          g0->SetPoint(fibre.number-1, (fibre.x*0.001), (fibre.y+4800.));
           count++;
           stream.readLine();
       }
   }
   textFile.close();
 
-  textFile.setFileName("/home/iwanicki/sw/opticalCon/data/4TSAACFIM00127/scan/long.csv");
-  count=0;
-  if (textFile.open(QIODevice::ReadOnly)) {
-      QTextStream stream(&textFile);
-      while (!stream.atEnd())
-      {
-          count++;
-          stream.readLine();
-      }
-  } else {
-      qDebug() << "cannot open file !!";
-      return 0;
-  }
-  textFile.close();
-
-  const int nFibresLong = count;
-  TGraph* gl = new TGraph(nFibresLong);
+  textFile.setFileName("/home/iwanicki/sw/opticalCon/data/4TSAACFIM00127/reproducibility_scan/2.csv");
   count = 0;
   if (textFile.open(QIODevice::ReadOnly)) {
       QTextStream stream(&textFile);
       while (!stream.atEnd())
       {
           coordinate fibre;
-          double temp;
-          stream >> fibre.number >> temp
-                  >> fibre.x >> fibre.y;
-          qDebug() << "longFiber" << fibre.number << fibre.x << fibre.y;
+          double dump;
+          stream >> fibre.number >> dump
+                    >> fibre.x >> fibre.y;
+          //qDebug() << "shortFiber" << fibre.number << fibre.x << fibre.y;
           if (fibre.number != (count+1))
               qDebug() << "fibre " << count << " skipped!";
-          gl->SetPoint(fibre.number-1, -(fibre.x*0.001), (fibre.y+4800.));
+          g1->SetPoint(fibre.number-1, (fibre.x*0.001), (fibre.y+4800.));
+          count++;
+          stream.readLine();
+      }
+  }
+  textFile.close();
+
+  textFile.setFileName("/home/iwanicki/sw/opticalCon/data/4TSAACFIM00127/reproducibility_scan/3.csv");
+  count = 0;
+  if (textFile.open(QIODevice::ReadOnly)) {
+      QTextStream stream(&textFile);
+      while (!stream.atEnd())
+      {
+          coordinate fibre;
+          double dump;
+          stream >> fibre.number >> dump
+                    >> fibre.x >> fibre.y;
+          //qDebug() << "shortFiber" << fibre.number << fibre.x << fibre.y;
+          if (fibre.number != (count+1))
+              qDebug() << "fibre " << count << " skipped!";
+          g2->SetPoint(fibre.number-1, (fibre.x*0.001), (fibre.y+4800.));
+          count++;
+          stream.readLine();
+      }
+  }
+  textFile.close();
+
+  textFile.setFileName("/home/iwanicki/sw/opticalCon/data/4TSAACFIM00127/reproducibility_scan/4.csv");
+  count = 0;
+  if (textFile.open(QIODevice::ReadOnly)) {
+      QTextStream stream(&textFile);
+      while (!stream.atEnd())
+      {
+          coordinate fibre;
+          double dump;
+          stream >> fibre.number >> dump
+                    >> fibre.x >> fibre.y;
+          //qDebug() << "shortFiber" << fibre.number << fibre.x << fibre.y;
+          if (fibre.number != (count+1))
+              qDebug() << "fibre " << count << " skipped!";
+          g3->SetPoint(fibre.number-1, (fibre.x*0.001), (fibre.y+4800.));
+          count++;
+          stream.readLine();
+      }
+  }
+  textFile.close();
+
+  textFile.setFileName("/home/iwanicki/sw/opticalCon/data/4TSAACFIM00127/reproducibility_scan/5.csv");
+  count = 0;
+  if (textFile.open(QIODevice::ReadOnly)) {
+      QTextStream stream(&textFile);
+      while (!stream.atEnd())
+      {
+          coordinate fibre;
+          double dump;
+          stream >> fibre.number >> dump
+                    >> fibre.x >> fibre.y;
+          //qDebug() << "shortFiber" << fibre.number << fibre.x << fibre.y;
+          if (fibre.number != (count+1))
+              qDebug() << "fibre " << count << " skipped!";
+          g4->SetPoint(fibre.number-1, (fibre.x*0.001), (fibre.y+4800.));
           count++;
           stream.readLine();
       }
@@ -85,21 +134,38 @@ int main(int argc, char** argv) {
 
   TCanvas* c1 = new TCanvas("c1", "c1");
   c1->cd();
-  gs->GetXaxis()->SetTitle("x [mm]");
-  gs->GetYaxis()->SetTitle("y [#mum]");
-  gs->GetXaxis()->SetTitleOffset(1.25);
-  gs->GetXaxis()->SetTitleSize(25);
-  gs->GetYaxis()->SetTitleOffset(1.15);
-  gs->GetYaxis()->SetTitleSize(25);
-  gs->SetMarkerStyle(kCircle);
-  gs->SetMarkerColor(kBlue);
-  gs->SetTitle("");
-  //gs->GetYaxis()->SetRangeUser(8.,26.);
-  //gs->GetXaxis()->SetLimits(0.,230.);
-  gs->Draw("ap");
-  gl->SetMarkerStyle(8);
-  gl->SetMarkerSize(0.5);
-  gl->Draw("p same");
+  g0->GetXaxis()->SetTitle("x [mm]");
+  g0->GetYaxis()->SetTitle("y [#mum]");
+  g0->GetXaxis()->SetTitleOffset(1.25);
+  g0->GetXaxis()->SetTitleSize(25);
+  g0->GetYaxis()->SetTitleOffset(1.15);
+  g0->GetYaxis()->SetTitleSize(25);
+  g0->SetMarkerStyle(8);
+  g0->SetMarkerSize(0.5);
+  g0->SetTitle("");
+  //g0->GetYaxis()->SetRangeUser(8.,26.);
+  //g0->GetXaxis()->SetLimits(0.,230.);
+  g0->Draw("ap");
+
+  g1->SetMarkerStyle(8);
+  g1->SetMarkerSize(0.5);
+  g1->SetMarkerColor(kBlue);
+  g1->Draw("p same");
+
+  g2->SetMarkerStyle(8);
+  g2->SetMarkerSize(0.5);
+  g2->SetMarkerColor(kRed);
+  g2->Draw("p same");
+
+  g3->SetMarkerStyle(8);
+  g3->SetMarkerSize(0.5);
+  g3->SetMarkerColor(kYellow);
+  g3->Draw("p same");
+
+  g4->SetMarkerStyle(8);
+  g4->SetMarkerSize(0.5);
+  g4->SetMarkerColor(kMagenta);
+  g4->Draw("p same");
 
   /*
   TLegend* l1 = new TLegend(.8,.8,.9,.9);
